4d39d38be9f87dbc43e4c47bec12c018d6899e8c
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2018-12-04 15:34:27 +0200

    GUI

---
+++ src/GUI/Main.java
@@ -0,0 +1,17 @@
+package GUI;
+
+
+import javax.swing.JFrame;
+
+
+public class Main 
+{
+	public static void main(String[] args)
+	{
+		MainWindow window = new MainWindow();
+		window.setVisible(true);
+		window.setSize(window.myImage.getWidth(),window.myImage.getHeight());
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
---
+++ src/GUI/MainWindow.java
@@ -0,0 +1,96 @@
+package GUI;
+
+
+import java.awt.Graphics;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+
+public class MainWindow extends JFrame implements MouseListener
+{
+	public BufferedImage myImage;
+	
+	public MainWindow() 
+	{
+		initGUI();		
+		this.addMouseListener(this); 
+	}
+	
+	private void initGUI() 
+	{
+		MenuBar menuBar = new MenuBar();
+		Menu menu = new Menu("Menu"); 
+		MenuItem item1 = new MenuItem("menu item 1");
+		MenuItem item2 = new MenuItem("menu item 2");
+		
+		menuBar.add(menu);
+		menu.add(item1);
+		menu.add(item2);
+		this.setMenuBar(menuBar);
+		
+		try {
+			 myImage = ImageIO.read(new File("Ariel1.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+	}
+
+	int x = -1;
+	int y = -1;
+
+	public void paint(Graphics g)
+	{
+		g.drawImage(myImage, 0, 0, this);
+	
+		if(x!=-1 && y!=-1)
+		{
+			int r = 10;
+			x = x - (r / 2);
+			y = y - (r / 2);
+			g.fillOval(x, y, r, r);
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg) {
+		System.out.println("mouse Clicked");
+		System.out.println("("+ arg.getX() + "," + arg.getY() +")");
+		x = arg.getX();
+		y = arg.getY();
+		repaint();
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		System.out.println("mouse entered");
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
