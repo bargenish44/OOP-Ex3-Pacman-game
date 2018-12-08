a607bfeaf010eceec90d4fc0a1511aea826e334e
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2018-12-08 16:44:11 +0200

    icons

--- src/Map/TestImagePane.java
+++ src/Map/TestImagePane.java
@@ -1,14 +1,20 @@
 package Map;
 
-
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.imageio.ImageIO;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class TestImagePane {
+	private ArrayList<Packman>Packmanarr=new ArrayList<>();
+	private ArrayList<Fruit>Fruitarr=new ArrayList<>();
+	private ImageIcon packmanimage;
+	private ImageIcon cherryimage;
+	private int counter=0;
+	private int count=0;
 	public static void main(String[] args) {
 		new TestImagePane();
 	}
@@ -25,7 +31,8 @@ public class TestImagePane {
 				try {
 					Image img = null;
 					img = ImageIO.read(new File("Ariel1.png"));
-
+					packmanimage=new ImageIcon("pacman.jpg");
+					cherryimage=new ImageIcon("cherry.png");
 					JFrame frame = new JFrame("Testing");
 					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.setLayout(new BorderLayout());
@@ -82,24 +89,38 @@ public class TestImagePane {
 			double y = getHeight();
 			lat = ((y/y/180)-90)/-1;
 			lon=x/(x/360)-180;
-			System.out.println("[lon: " + lon + " lat: " + lat + "]: X: " + x + " Y: " + y);
-			g.setColor(Color.RED);
-			g.drawOval((int) x, (int) y, 5, 5);
-
+			//System.out.println("[lon: " + lon + " lat: " + lat + "]: X: " + x + " Y: " + y);
+			for(int i=0;i<Packmanarr.size();i++) {
+				g.drawImage(packmanimage.getImage(), Packmanarr.get(i).getOrinet().ix(), Packmanarr.get(i).getOrinet().iy(),50,50,null);
+			}
+			for(int i=0;i<Fruitarr.size();i++) {
+				g.drawImage(cherryimage.getImage(), Fruitarr.get(i).getOrient().ix(), Fruitarr.get(i).getOrient().iy(),50,50,null);
+			}
 		}
-
 		@Override
 		public void mouseClicked(MouseEvent e) {
 			if(e.getButton() == MouseEvent.BUTTON1) {
-				x = e.getX()/lat;
-				y = e.getY()/lon;
+				x = e.getX();
+				y = e.getY();
 				System.out.println("left click X: " + x + " Y: " + y);
+				 String test1= JOptionPane.showInputDialog("Please input packman speed : ");
+			        String test2= JOptionPane.showInputDialog("Please input packman radius : ");
+			        double speed=Integer.parseInt(test1);
+					double radius=Integer.parseInt(test2);
+				Packmanarr.add(new Packman(x,y,0,speed,radius,count));
+				count++;
 				repaint();	
 			}
 			else {
-				x = e.getX()/lat;
-				y = e.getY()/lon;
+				x = e.getX();
+				y = e.getY();
 				System.out.println("right click X: " + x + " Y: " + y);
+				 String test1= JOptionPane.showInputDialog("Please input fruit weight : ");
+			        String test2= JOptionPane.showInputDialog("Please input fruit radius : ");
+			        double weight=Integer.parseInt(test1);
+					double radius=Integer.parseInt(test2);
+				Fruitarr.add(new Fruit(counter,x,y,0,weight,radius));
+				counter++;
 				repaint();			
 			}
 		}
@@ -128,5 +149,4 @@ public class TestImagePane {
 
 		}
 	}
-
 }
