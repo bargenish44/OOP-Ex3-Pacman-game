c1b1ffab75bd01029bf74535dad53cce39429855
bargenish44 <44568486+bargenish44@users.noreply.github.com>
2018-12-17 20:19:04 +0200

    Update ShortestPathAlg.java

--- src/Map/ShortestPathAlg.java
+++ src/Map/ShortestPathAlg.java
@@ -13,19 +13,12 @@ public class ShortestPathAlg {
 	public ShortestPathAlg(ArrayList<Packman>arr,ArrayList<Fruit> array) {
 		setGame(new Game(arr,array));
 	}
-
 	private static double Calculatetime(Packman p,Fruit f) {
 		Circle c=new Circle(p.getOrinet(),p.getRadius());
 		double dist=Map.distance3d(c.get_cen(),f.getOrient())-c.get_radius();
 		if(dist<=0)return 0;
 		return dist/p.getSpeed();
 	}
-	//	public static void main(String[] args) {
-	//		Point3D p=new Point3D(1,8,8);
-	//		Point3D p2=new Point3D(5,4,4);
-	//		Circle c=new Circle(p, 2);
-	//		System.out.println(c.distance3D(p2));
-	//	}
 	public double Shortalgo(Game g) {
 		return greedy(g);
 	}
@@ -48,27 +41,33 @@ public class ShortestPathAlg {
 					}
 				}
 			}
-			//			System.out.println(tmp);
 			fruittmp=g.getArray().get(fruitindex);
 			g.getArr().get(packmanindex).setScore(g.getArray().get(fruitindex).getWeight());
-			g.getArr().get(packmanindex).getPath().getArr().add(fruittmp.getOrient());
-			g.getArr().get(packmanindex).setOrinet(fruittmp.getOrient());
-			//			Point3D p=calcvector(g.getArr().get(packmanindex).getOrinet(),fruittmp.getOrient());
-			//			p=normalvector(p);
-			//			Point3D temp=g.getArr().get(packmanindex).getOrinet();
+//			g.getArr().get(packmanindex).getPath().getArr().add(fruittmp.getOrient());
+//			g.getArr().get(packmanindex).setOrinet(fruittmp.getOrient());
+			Point3D packmantmp=g.getArr().get(packmanindex).getOrinet();
+			Point3D p=calcvector(g.getArr().get(packmanindex).getOrinet(),fruittmp.getOrient());
+			p=normalvector(p);
+			Point3D temp=g.getArr().get(packmanindex).getOrinet();
 			time+=min;
-			//			while(min>0) {
-			//				temp.add(p);
-			//				g.getArr().get(packmanindex).getPath().getArr().add(temp);
-			//				g.getArr().get(packmanindex).setOrinet(temp);
-			//				min=Calculatetime(g.getArr().get(packmanindex),g.getArray().get(fruitindex));
-			//				System.out.println(min+" min");
-			//				time+=min;
-			//			}
+			for(double i=packmantmp.x();i<=fruittmp.getOrient().x();i+=p.x()) {
+				temp.add(p);
+				g.getArr().get(packmanindex).getPath().getArr().add(temp);
+				g.getArr().get(packmanindex).setOrinet(temp);
+				//				min=Calculatetime(g.getArr().get(packmanindex),g.getArray().get(fruitindex));
+			}
 			g.getArray().remove(fruitindex);
 			min=Double.MAX_VALUE;
 			System.out.println(time);
 		}
+		//			while(min>0) {
+		//				temp.add(p);
+		//				g.getArr().get(packmanindex).getPath().getArr().add(temp);
+		//				g.getArr().get(packmanindex).setOrinet(temp);
+		//				min=Calculatetime(g.getArr().get(packmanindex),g.getArray().get(fruitindex));
+		//				System.out.println(min+" min");
+		//				time+=min;
+		//			}
 		return time;
 	}
 	public Game getGame() {
@@ -77,20 +76,12 @@ public class ShortestPathAlg {
 	public void setGame(Game game) {
 		this.game = game;
 	}
-	//	public static Point3D calcvector(Point3D p,Point3D p2) {
-	//		return new Point3D(p2.x()-p.x(),p2.y()-p.y(),p2.z()-p.z());
-	//	}
-	//	public static Point3D normalvector(Point3D vector) {
-	//		double sum=vector.x()*vector.x()+vector.y()*vector.y()+vector.z()*vector.z();
-	//		sum=Math.sqrt(sum);
-	//		return new Point3D(vector.x()/sum,vector.y()/sum,vector.z()/sum);
-	//	}
-	//	public static void main(String[] args) {
-	//		Point3D p=new Point3D(1,4,6);
-	//		Point3D p2=new Point3D(5,8,8);
-	//		Point3D p3=calcvector(p,p2);
-	//		System.out.println(p3.toString());
-	//		p3=normalvector(p3);
-	//		System.out.println(p3.toString());
-	//	}
+	public static Point3D calcvector(Point3D p,Point3D p2) {
+		return new Point3D(p2.x()-p.x(),p2.y()-p.y(),p2.z()-p.z());
+	}
+	public static Point3D normalvector(Point3D vector) {
+		double sum=vector.x()*vector.x()+vector.y()*vector.y()+vector.z()*vector.z();
+		sum=Math.sqrt(sum);
+		return new Point3D(vector.x()/sum,vector.y()/sum,vector.z()/sum);
+	}
 }
