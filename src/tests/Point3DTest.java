f76465dfaccdbb59890fca1aeb14ae5ae0fd4dc2
elyashiv <elyashivderi17@gmail.com>
2018-12-17 16:20:29 +0200

    fix test point3D

---
+++ src/tests/Point3DTest.java
@@ -0,0 +1,87 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import Geom.Point3D;
+
+class Point3DTest {
+
+
+	@Test
+	void testAddPoint3D() {
+		
+		Point3D test1 = new Point3D(4,2,0);
+		
+		Point3D test2 = new Point3D(3,2,0);
+		
+		Point3D ans = new Point3D(7,4,0);
+		
+		test1.add(test2);
+
+		if(!ans.toString().equals(test1.toString()))
+
+			fail("Error: Insert incorrect");
+	}
+
+	@Test
+	void testDistance3DPoint3D() {
+
+		Point3D test1=new Point3D(32.103315,35.209039,670);
+		
+		Point3D test2=new Point3D(32.106352,35.205225,650);
+
+		if(test1.distance3D(test2)!=20.000000594249116)
+
+			fail("ERROR: The distance calculation is incorrect");
+	}
+
+	@Test
+	void testRescalePoint3DPoint3D() {
+		
+		Point3D test1 = new Point3D(2.5,3.0,0);
+
+		Point3D test2 = new Point3D(5.5,4.5,0);
+
+		Point3D test3 = new Point3D(3.5,1,0);
+		
+		test3.rescale(test1, test2);
+
+		Point3D ans = new Point3D(8.0,-6.0,0.0);
+
+		if(!ans.toString().equals(test3.toString()))
+
+			fail("Error: Wrong calculation");
+	}
+
+	
+	@Test
+	void testAngleXY() {
+		
+		Point3D test1 = new Point3D(2.5,3.0,0);
+
+		Point3D test2 = new Point3D(5.5,4.5,0);
+		
+		double ans = test2.angleXY(test1);
+		
+		if(ans!=-2.677945044588987)
+		
+		fail("Error: Wrong calculation");
+	}
+
+	@Test
+	void testNorth_angle() {
+		
+		Point3D test1 = new Point3D(2.5,3.0,0);
+
+		Point3D test2 = new Point3D(5.5,4.5,0);
+		
+		double ans = test2.north_angle(test1);
+		
+		if(ans!=243.43494882292202)
+		
+		fail("Error: Wrong calculation");
+	}
+
+}
