42dd74c215d72283ef05535c0909cb154c5a2c67
elyashiv <elyashivderi17@gmail.com>
2018-12-24 00:05:15 +0200

    fix test myframe

--- src/tests/MyFrameTest.java
+++ src/tests/MyFrameTest.java
@@ -2,13 +2,18 @@ package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.awt.event.ActionEvent;
+import java.sql.Time;
 import java.util.ArrayList;
 import org.junit.jupiter.api.Test;
+
+import Coords.MyCoords;
 import Packman_Game.MyFrame;
 import Packman_Game.Packman;
 
 
+
 class MyFrameTest {
+	private static final String M = null;
 	/**
 	 * MyFrame tests.
 	 * @author Bar Genish
@@ -16,15 +21,16 @@ class MyFrameTest {
 	 */
 	ArrayList<Packman> arr_test = new ArrayList<>();
 	Class<MyFrame> test_1 ;
-	MyFrame test_4 ;
-	MyFrame test_3 ;
+	//MyFrame test_4 ;
+	Class<MyCoords> test_3 ;
 	MyFrame test_7 ;
 	int x=7,y=3;
 	String test_2;
+	String test_8= M;
 	ArrayList<Packman> arr_test_2 = new ArrayList<>();
 
 
-	@Test
+	@Test 
 	void testMyFrame() {
 
 		test_1 = MyFrame.class;
@@ -34,8 +40,14 @@ class MyFrameTest {
 		if(test_1.equals(test_2))
 
 			fail("Not yet implemented");
+		
+		test_3 = MyCoords.class;
+		
+		if(test_3.equals(test_8))
+			
+			fail("Not yet implemented");
 	}
-
+		
 
 	@Test
 	void testActionPerformed() {
@@ -46,6 +58,12 @@ class MyFrameTest {
 
 		if(test_5 == test_6)
 
+			fail("Not yet implemented");
+		
+		MyFrame	test_0 = new MyFrame();
+		
+		if(test_0.equals(test_7))
+			
 			fail("Not yet implemented");
 	}
 
